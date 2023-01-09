4b5253f280c3a35c07356c9e898e26b79cad312a
vitormalencar <vitor.maia.alencar@gmail.com>
2023-01-09 22:13:48 +0100

    chore : updating store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,36 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import {
+  Action,
+  combineReducers,
+  configureStore,
+  PreloadedState,
+  ThunkAction,
+} from "@reduxjs/toolkit";
+import { apiSlice } from "../features/api/apiSlice";
+import { castMembersApiSlice } from "../features/cast/castMembersSlice";
+import { categoriesApiSlice } from "../features/categories/categorySlice";
+import { genreSlice } from "../features/genre/genreSlice";
+import { videosSlice } from "../features/videos/VideoSlice";
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+const rootReducer = combineReducers({
+  [apiSlice.reducerPath]: apiSlice.reducer,
+  [categoriesApiSlice.reducerPath]: apiSlice.reducer,
+  [castMembersApiSlice.reducerPath]: apiSlice.reducer,
+  [videosSlice.reducerPath]: apiSlice.reducer,
+  [genreSlice.reducerPath]: apiSlice.reducer,
 });
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(apiSlice.middleware),
+  });
+};
+
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore["dispatch"];
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
