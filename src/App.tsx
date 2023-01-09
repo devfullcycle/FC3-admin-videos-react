a08599eccc049757a2fc01981fbcd1367f9f72f9
vitormalencar <vitor.maia.alencar@gmail.com>
2023-01-09 22:14:18 +0100

    chore: adding index and app files

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./components/Layout";
+
+import { CreateCastMember } from "./features/cast/CreateCastMembers";
+import { EditCastMember } from "./features/cast/EditCastMember";
+import { ListCastmembers } from "./features/cast/ListCastmembers";
+import { CategoryCreate } from "./features/categories/CreateCategory";
+import { CategoryEdit } from "./features/categories/EditCategory";
+import { CategoryList } from "./features/categories/ListCaegory";
+import { GenreCreate } from "./features/genre/GenreCreate";
+import { GenreEdit } from "./features/genre/GenreEdit";
+import { GenreList } from "./features/genre/GenreList";
+import { VideosCreate } from "./features/videos/VideosCreate";
+import { VideosEdit } from "./features/videos/VideosEdit";
+import { VideosList } from "./features/videos/VideosList";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<CategoryList />} />
+
+        {/* Category */}
+        <Route path="/categories" element={<CategoryList />} />
+        <Route path="/categories/create" element={<CategoryCreate />} />
+        <Route path="/categories/edit/:id" element={<CategoryEdit />} />
+
+        {/* Cast members */}
+        <Route path="/cast-members" element={<ListCastmembers />} />
+        <Route path="/cast-members/create" element={<CreateCastMember />} />
+        <Route path="/cast-members/edit/:id" element={<EditCastMember />} />
+
+        {/* Genre */}
+        <Route path="/genres" element={<GenreList />} />
+        <Route path="/genres/create" element={<GenreCreate />} />
+        <Route path="/genres/edit/:id" element={<GenreEdit />} />
+
+        {/* Videos */}
+        <Route path="/videos" element={<VideosList />} />
+        <Route path="/videos/create" element={<VideosCreate />} />
+        <Route path="/videos/edit/:id" element={<VideosEdit />} />
+
+        <Route
+          path="*"
+          element={
+            <Box sx={{ color: "white" }}>
+              <Typography variant="h1">404</Typography>
+              <Typography variant="h2">Page not found</Typography>
+            </Box>
+          }
+        />
+      </Routes>
+    </Layout>
   );
 }
 
