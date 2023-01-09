ac4d0c1495a5a9ba0e676591e5768f34a5850b78
vitormalencar <vitor.maia.alencar@gmail.com>
2023-01-09 22:12:55 +0100

    chore: adding config

---
+++ src/config/theme.ts
@@ -0,0 +1,21 @@
+import { createTheme } from "@mui/material";
+
+export const darkTheme = createTheme({
+  palette: {
+    background: { default: "#222222" },
+    mode: "dark",
+    primary: { main: "#f5f5f1" },
+    secondary: { main: "#E50914" },
+    text: { primary: "#f5f5f1" },
+  },
+});
+
+export const lightTheme = createTheme({
+  palette: {
+    background: {},
+    mode: "light",
+    primary: { main: "#E50914" },
+    secondary: { main: "#000" },
+    text: { primary: "#000" },
+  },
+});
